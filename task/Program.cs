82a21ab3399ffca5e75080806d7c385fc86aa069
Maxim Alexandrovsky <Hi136maksim11@mail.ru>
2022-12-27 14:34:35 +0400

    Add task

---
+++ task/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] enterArr = new string[7] {"hello", "world", "hi", "123", "7","name" ,"gb"};
+string[] exitArr = new string[enterArr.Length];
+
+arrFiltering(enterArr, exitArr);
+void arrFiltering(string[] enterArr, string[] exitArr)
+{
+    int count = 0;
+    for (int i = 0; i < enterArr.Length; i++)
+    {
+        if (enterArr[i].Length <= 3)
+        {
+            exitArr[count] = enterArr[i];
+            count++;
+        }
+    }
+}
+
+PrintArray(exitArr);
+void PrintArray(string[] exitArr)
+{
+    for (int i = 0; i < exitArr.Length; i++)
+    {
+        Console.Write($"{exitArr[i]} ");
+    }
+    Console.WriteLine();
+}
